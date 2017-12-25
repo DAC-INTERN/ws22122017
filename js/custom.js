cec89819ec6f1a753ce914a27ad26f32f6ac33fb
Wolf <cgvnd123@gmail.com>
2017-12-25 14:21:09 +0700

    fix loop because of Search() string,  update front end using template,  validate special character in the last, add * / button @todo In search UX , validate * / and type from keyboard

--- js/custom.js
+++ js/custom.js
@@ -10,28 +10,40 @@ var app = new Vue({
         },
 
         calculateResult: function () {
-            this.result = math.eval(this.validateMathString(this.mathString));
+            this.result = math.eval(this.validateMathString(this.mathString.toString()));
         },
 
         deleteMathString: function () {
-            this.mathString = this.mathString.slice(0,-1);
+            this.mathString = this.mathString.slice(0, -1);
         },
 
-        clearMathString: function () {
+        clearScrint: function () {
             this.mathString = '';
+            this.result = '';
         },
 
         validateMathString: function (mathString) {
             // remove invalid ..
             //@Todo: this function is not completed
-            while(mathString.search('..')!== -1){
-                // console.log("ok");
-                mathString = mathString.replace('..', '.');
-                console.log(mathString);
+            if (mathString.length > 13) {
+
+                alert("Sorry no more input is allowed");
+            } else {
+                while (mathString.indexOf('..') !== -1) {
+                    mathString = mathString.replace('..', '.');
+                }
+                while (mathString.substr(-1) === '+'
+                    || mathString.substr(-1) === '-'
+                    || mathString.substr(-1) === '*'
+                    || mathString.substr(-1) === '/') {
+
+                    mathString = mathString.slice(0, -1);
+                }
+                return mathString;
+
 
             }
-            return mathString;
-
         }
     }
+
 });
