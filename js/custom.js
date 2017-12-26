74f5180e2ea51f4e355229e86384ceef74fd7e34
Wolf <cgvnd123@gmail.com>
2017-12-26 14:35:03 +0700

    Complete validate function, add validate *,/ after other operator

--- js/custom.js
+++ js/custom.js
@@ -17,7 +17,7 @@ var app = new Vue({
             this.mathString = this.mathString.slice(0, -1);
         },
 
-        clearScrint: function () {
+        clearScreen: function () {
             this.mathString = '';
             this.result = '';
         },
@@ -25,13 +25,20 @@ var app = new Vue({
         validateMathString: function (mathString) {
             // remove invalid ..
             //@Todo: this function is not completed
+            //@Todo: add type from keyboard
+
+            // Alert when more than 13 character not to make full screen
             if (mathString.length > 13) {
 
                 alert("Sorry no more input is allowed");
             } else {
+
+                // Validate more than one dot
                 while (mathString.indexOf('..') !== -1) {
                     mathString = mathString.replace('..', '.');
                 }
+
+                //Validate operator in the last
                 while (mathString.substr(-1) === '+'
                     || mathString.substr(-1) === '-'
                     || mathString.substr(-1) === '*'
@@ -39,9 +46,23 @@ var app = new Vue({
 
                     mathString = mathString.slice(0, -1);
                 }
+
+                // Validate *,/ after other operator
+                var i = 0;
+                while (i < mathString.length) {
+                    if (mathString.charAt(i) === '+'
+                        || mathString.charAt(i) === '-'
+                        || mathString.charAt(i) === '*'
+                        || mathString.charAt(i) === '/') {
+
+                        if (mathString.charAt(i + 1) === '*'
+                            || mathString.charAt(i + 1) === '/') {
+                            alert("More than one operator side by side at char " + (i+1) + ", Try again, Please!");
+                            break;
+                        } else i++
+                    } else i++
+                }
                 return mathString;
-
-
             }
         }
     }
